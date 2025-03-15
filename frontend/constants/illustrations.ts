27ca6b47b5a19477ebf6d38f3fa7cca83c45b07e
johnb <jban0018@student.monash.edu>
2025-03-15 18:00:34 +1100

    Try my best

--- frontend/constants/illustrations.ts
+++ frontend/constants/illustrations.ts
@@ -2,6 +2,7 @@ const ILLUSTRATIONS = {
     dressgirl: "/illustrations/dressgirl.svg",
     clotheshanger: "/illustrations/homebutton1.svg",
     mirrorlady: "/illustrations/homebutton2.svg",
+    mannequin: "/illustrations/manni.svg",
   };
   
   export default ILLUSTRATIONS;
