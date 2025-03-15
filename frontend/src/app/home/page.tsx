5a866ba2d75d96b0153dffa9778d1977db18f5fb
jason301c <jasonalex.st@gmail.com>
2025-03-16 01:52:14 +1100

    Link lookbook correctly in home

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -31,14 +31,16 @@ export default function Home() {
           </button>
         </Link>
 
-        <button className="w-full py-10 px-6 flex justify-between items-center text-lg font-medium text-white rounded-lg shadow-md bg-prim-darkest hover:bg-prim-neutral relative overflow-hidden">
-          <span className="z-10">Look Book</span>
-          <img
-            src={ILLUSTRATIONS.lookbook}
-            alt="Icon"
-            className="absolute right-[10px] top-0 h-full min-w-[150px] object-cover object-right rounded-r-lg"
-          />
-        </button>
+        <Link href="/lookbook" className="w-full">
+          <button className="w-full py-10 px-6 flex justify-between items-center text-lg font-medium text-white rounded-lg shadow-md bg-prim-darkest hover:bg-prim-neutral relative overflow-hidden">
+            <span className="z-10">Look Book</span>
+            <img
+              src={ILLUSTRATIONS.lookbook}
+              alt="Icon"
+              className="absolute right-[10px] top-0 h-full min-w-[150px] object-cover object-right rounded-r-lg"
+            />
+          </button>
+        </Link>
 
         <Link href="/livelook" className="w-full">
           <button className="w-full py-4 px-6 flex justify-between items-center text-lg font-medium bg-green-500 text-white rounded-lg shadow-md bg-prim-darkest hover:bg-prim-neutral">
