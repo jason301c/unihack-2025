556112212f434a69d5c03d0c80805259a5cdf532
johnb <jban0018@student.monash.edu>
2025-03-15 13:08:38 +1100

    Finish the homepage

--- frontend/constants/illustrations.ts
+++ frontend/constants/illustrations.ts
@@ -1,5 +1,7 @@
 const ILLUSTRATIONS = {
     dressgirl: "/illustrations/dressgirl.svg",
+    clotheshanger: "/illustrations/homebutton1.svg",
+    mirrorlady: "/illustrations/homebutton2.svg",
   };
   
   export default ILLUSTRATIONS;
