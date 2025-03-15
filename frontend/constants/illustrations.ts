cd6381b3362801f8728672d79063ef070200a987
zhas0007 <zhas0007@student.monash.edu>
2025-03-15 21:44:29 +1100

    fixed homepage and added lookbook button.

--- frontend/constants/illustrations.ts
+++ frontend/constants/illustrations.ts
@@ -1,11 +1,11 @@
 const ILLUSTRATIONS = {
-    dressgirl: "/illustrations/dressgirl.svg",
-    clotheshanger: "/illustrations/homebutton1.svg",
-    mirrorlady: "/illustrations/homebutton2.svg",
-    mannequin: "/illustrations/manni.svg",
-    shrugMan: "/illustrations/shrugman.svg",
-    pianoGirl: "/illustrations/pianogirl.svg",
-  };
-  
-  export default ILLUSTRATIONS;
-  
+  dressgirl: "/illustrations/dressgirl.svg",
+  clotheshanger: "/illustrations/homebutton1.svg",
+  mirrorlady: "/illustrations/homebutton2.svg",
+  mannequin: "/illustrations/manni.svg",
+  shrugMan: "/illustrations/shrugman.svg",
+  pianoGirl: "/illustrations/pianogirl.svg",
+  lookbook: "/illustrations/Lookbook.svg",
+};
+
+export default ILLUSTRATIONS;
