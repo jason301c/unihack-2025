af0a79a1ad6589876ed512728bcac53e4648e9e4
jason301c <jasonalex.st@gmail.com>
2025-03-15 13:25:59 +1100

    Import colors from Figma to tailwind config (for light mode)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                // Imported from Figma
+                'prim-lightest': '#FAFAFA', // Very Light Gray
+                'prim-light': '#E4E5F1',    // Light Gray
+                'prim-neutral': '#D2D3DB',          // Gray
+                'prim-dark': '#9394A4',     // Dark Gray
+                'prim-darkest': '#494B6A',  // Very Dark Gray
+                'prim-default': '#36374E',       // Darkest
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
